1d43582006314b7b4b4414f1de4227b5d73c7077
PedroQueiroz1 <pedroqueiroz0411@outlook.com.br>
2022-07-12 17:14:31 -0300

    Adds aula11

---
+++ aula11/index.js
@@ -0,0 +1,23 @@
+// Operadores aritméticos - / *
+// + adição | concatenação
+// ** Potenciação
+// % resto da divisão
+
+// Incremento = ++ 
+// Decremento = -
+// +=z
+
+// ORDEM: ()    ->   **   ->   * | / | %   ->   + | -
+
+let contador = 1;
+contador++;
+++contador;
+
+console.log(contador);
+
+const num1 = 10;
+const num2 = parseInt(`5`);
+const num3 = parseFloat(`5.3`);
+const num4 = Number(`5.4`);
+console.log(num1+num2);
+console.log(num3+num4);
