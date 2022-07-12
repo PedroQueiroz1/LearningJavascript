521a4dc2708f8899d54a0c4c9502bdc23c90f61c
PedroQueiroz1 <pedroqueiroz0411@outlook.com.br>
2022-07-12 15:14:04 -0300

    Adds aula06

---
+++ aula06/index.js
@@ -0,0 +1,20 @@
+let nome01 = `João`;
+var nome02 = `João`;
+
+// Não criar variáveis com palavras reservadas
+// Variáveis precisam ter nomes significativos
+// Não pode começar com número, o nome da variável
+// Case-sensitive
+// Não conter espaços/traços
+// camelCase
+// NÃO UTILIZAR VAR, UTILIZAR LET.
+// Não podemos REDECLARAR variáveis com let
+
+console.log(nome01,`nasceu em 1984.`);
+console.log(`Em 2000,`,nome01,`conheceu Maria.`);
+console.log(`%s casou-se com Maria em 2012.`,nome01);
+console.log(`Maria teve 1 filho com João em 2015.`);
+console.log(`O filho de João se chama Eduardo.`);
+
+nome01=`Otávio`;
+console.log(nome01);
