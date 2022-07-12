4560ed95a591837c5341b53f3ca065532e4a1898
PedroQueiroz1 <pedroqueiroz0411@outlook.com.br>
2022-07-12 16:28:19 -0300

    Adds aula10

---
+++ aula10/index.js
@@ -0,0 +1,7 @@
+const nome = `Pedro`; // string " " | ' ' | ` `
+const num1 = 10; // number 10 | 10.52
+let nomeAluno; // undefined = não aponta pra nenhum local na memória
+let sobrenomeAluno = null; // Nulo = não aponta pra nenhum local na memória
+const aprovado = true; // Boolean true | false  (valor lógico)
+
+//Symbol ...
