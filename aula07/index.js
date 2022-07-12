5b6444149bb8f99ce6368ba25aa0bfcaabeac1b2
PedroQueiroz1 <pedroqueiroz0411@outlook.com.br>
2022-07-12 15:28:45 -0300

    Adds aula07

---
+++ aula07/index.js
@@ -0,0 +1,17 @@
+// Não criar constantes com palavras reservadas
+// Constantes precisam ter nomes significativos
+// Não pode começar com número, o nome da constante
+// Case-sensitive
+// Não conter espaços/traços
+// camelCase
+// NÃO UTILIZAR VAR, UTILIZAR CONST.
+// Não podemos REDECLARAR constante com const.
+
+const nome = `João`;
+const primeiroNumero = 1;
+const segundoNumero = 2;
+const conta = primeiroNumero + segundoNumero;
+
+console.log(conta);
+console.log(typeof(primeiroNumero+segundoNumero));
+console.log(nome);
