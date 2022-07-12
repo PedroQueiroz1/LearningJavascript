e1aeeaeb63752fa3318c10af16e0cc2054540e58
PedroQueiroz1 <pedroqueiroz0411@outlook.com.br>
2022-07-12 16:15:12 -0300

    Adds aula09

---
+++ aula09/index.js
@@ -0,0 +1,11 @@
+// ECMAScript 2015 (ES6) = padronização do javascript
+
+var nome = `Luiz`;
+var nome = `Pedro`;
+
+console.log(nome);
+
+let nome02 = `Pedro`;
+nome02 = `Greg`;
+
+console.log(nome02);
