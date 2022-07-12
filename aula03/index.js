eac4d6d88ffe46fada8b6e3b3fc4c85cf6bf02f3
PedroQueiroz1 <pedroqueiroz0411@outlook.com.br>
2022-07-12 14:23:37 -0300

    Adds 'aula03' + 'aula04'

---
+++ aula03/index.js
@@ -0,0 +1,3 @@
+var nome = `Pedro`;
+var horario = 14;
+console.log(`Olá, meu nome é "%s". Estou aprendendo Javascript às %s da tarde.`,nome,horario);
---
+++ aula04/comentarios.js
@@ -0,0 +1,5 @@
+// E escrever um comentário
+console.log(`Olá mundo!`); // Aqui tem outro comentário
+/* Comentário
+console.log(`Linha 1`);
+*/
