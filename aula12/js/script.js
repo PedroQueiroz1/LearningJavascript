775d799df24bfd79069e52d0e82f5a967c9bea3b
PedroQueiroz1 <pedroqueiroz0411@outlook.com.br>
2022-07-12 23:23:14 -0300

    Adds aula12

---
+++ aula12/js/script.js
@@ -0,0 +1,6 @@
+let num1 = prompt("Digite o num1");
+let num2 = prompt("Digite o num2");
+num1 = Number(num1);
+num2 = Number(num2);
+const resultado = num1 + num2;
+alert("Resultado: " + resultado); // ${num1 + num2}
