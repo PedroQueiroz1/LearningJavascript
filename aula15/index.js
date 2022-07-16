3dd2a5c8961e2864d15752a391ecf93a58c7b565
PedroQueiroz1 <pedroqueiroz0411@outlook.com.br>
2022-07-15 23:13:52 -0300

    Adds aula15

---
+++ aula15/index.js
@@ -0,0 +1,17 @@
+let num1 = 1;
+let num2 = 2.44355;
+let num3 = 0.1;
+let num4 = 0.7;
+
+num3+=num4;//0.8
+num3+=num4;//0.9
+num3+=num4;//1.0
+
+num3 = parseFloat(num3.toFixed(2));
+
+
+//console.log(num1.toString()+num2);
+//num1 = num1.toString();
+//console.log(num1.toString(2)); //(2) -> binário
+//console.log(num2.toFixed(2)); //qtd de casas decimais
+//console.log(Number.isInteger(num1)); isNaN (not a number)
