499454f61ebec9963e7d11aa492a720591569066
PedroQueiroz1 <pedroqueiroz0411@outlook.com.br>
2022-07-16 21:15:11 -0300

    Adds aula16

---
+++ aula16/index.js
@@ -0,0 +1,10 @@
+let num1 = 9.42345
+let num2 = Math.floor(num1);
+let num3 = Math.ceil(num1);
+let num4 = Math.round(num1);
+let nums5 = [1,2,4,-50,40,1999];
+
+console.log(Math.max(nums5));
+console.log(Math.max(1,2,4,-50,40,1999)); //min();
+console.log(Math.PI); // Math.pow(2,10) -> 2 elevado a 10
+const aleatorio = Math.round(Math.random()*(10-5)+5);
