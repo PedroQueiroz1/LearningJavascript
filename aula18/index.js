2c80c4e16d302506e5463dd188f68a64e1b7ab32
PedroQueiroz1 <pedroqueiroz0411@outlook.com.br>
2022-07-20 22:50:29 -0300

    Adds aula18

---
+++ aula18/index.js
@@ -0,0 +1,15 @@
+/*function saudacao(nome){
+    console.log(`Bom dia, ${nome}!`);
+    //return 1234;
+}
+
+const variavel = saudacao('Pedro');*/
+
+function soma(a,b){
+    const resultado = a+b;
+    return resultado;
+}
+console.log(soma(4,2));
+
+const raiz = n => n**0.5;  // "arrow function"
+
