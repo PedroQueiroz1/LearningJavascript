00351d31e7311bf788917cff820f4c118f35f345
PedroQueiroz1 <pedroqueiroz0411@outlook.com.br>
2022-07-26 23:48:48 -0300

    Adds aula22

---
+++ aula22/index.js
@@ -0,0 +1,16 @@
+/*
+Operadores de comparação
+>
+>=
+<
+<=
+== igualdade (valor)
+=== igualdade escrita (valor e tipo)
+!= (valor)
+!== (valor e tipo)
+*/
+
+const num1 = 10;
+const num2 = '10';
+const comp = num1==num2; // RECOMENDADO ===
+console.log(comp) // TRUE
